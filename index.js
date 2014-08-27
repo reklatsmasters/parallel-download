1ded3a44ff9f32300639f733d668f9cab809b4ed
Дмитрий Цветцих <dmitrycvet@gmail.com>
2014-08-27 14:56:18 +0600

    [+] init

---
+++ index.js
@@ -0,0 +1,125 @@
+var parallel = require('run-parallel')
+	, request = require("request")
+	, assign = require('object-assign');
+	;
+
+/**
+ * @class
+ * @param {object} [opts] - Options for <request>
+ */
+function Downloader(opts) {
+	this.opts = opts || {};
+	this.opts.encoding = null;
+};
+
+Downloader.prototype = function() {
+
+	var scope = {
+		constructor: Downloader
+	};
+
+	var tasks = [];
+
+	var generator = {
+		get: function(url, opts) {
+			return function(cb) {
+				var data = [];
+
+				request.get(url, opts)
+				.on("error", function(err){
+					cb({url:url, error:err});
+				})
+				.on("data", function(chunk){
+					data.push(chunk);
+				})
+				.on('response', function (res) {
+					if (res.statusCode < 200 || res.statusCode >= 300) {
+						cb({url:url, error: new Error(res.statusCode)});
+						return;
+					}
+				})
+				.on("end", function() {
+					cb(null, {url:url, content:Buffer.concat(data)});
+				});
+			};
+		}
+	};
+
+	/* @typedef {string} Url */
+
+	/**
+	 * @param {Url|Url[]} url
+	 * @param {object} [opts] - Options for <request>
+	 */
+	scope.get = function(url, opts) {
+		opts = assign({}, this.opts, opts);
+
+		if (typeof url === "string") {
+			tasks.push( generator.get(url, opts) );
+		} else if (Array.isArray(url)) {
+			url.forEach(function(item){
+				tasks.push( generator.get(item, opts) );
+			});
+		}
+		return this;
+	};
+
+	/**
+	 * @typedef ResultHash
+	 * @type {object}
+	 * @property {Url} url
+	 * @property {Buffer} content - Downloaded data
+	 */
+
+	 /**
+	  * @typedef ErrorHash
+	  * @type {object}
+	  * @property {Url} url
+	  * @property {Error} error
+	  */
+
+	/**
+	 * @callback RunCallback
+	 * @param {null|ErrorHash[]} err - Array of errors
+	 * @param {ResultHash[]} result 
+	 */
+
+	/**
+	 * @param {RunCallback} cb
+	 */
+	scope.run = function(cb) {
+		parallel(tasks, function(err, out) {
+			cb(err, out.filter(function(item){ return !!item; }));
+		});
+	};
+
+	return scope;
+}.call(null);
+
+/**
+ * Simple parallel downloader
+ * @param {Url|Url[]} urls
+ * @param {object}  [opts]
+ * @param {RunCallback} cb  
+ */
+function download(urls, opts, cb) {
+	if (this instanceof download) {
+		opts = opts || urls;
+		return new Downloader(opts);
+	}
+
+	if (!Array.isArray(urls) && (typeof urls !== "string")) {
+		throw new TypeError("Expected url or array of urls");
+	}
+
+	if (typeof opts === "function") {
+		cb = opts;
+		opts = {};
+	}
+
+	var d = new Downloader(opts);
+	d.get(urls);
+	d.run(cb);
+}
+
+module.exports = download;
---
+++ readme.md
@@ -0,0 +1,3 @@
+# parallel-download
+
+> Parallel downloads files to the buffer.
