787b7486533adaa07b675052b6f5b8fb27df5b92
Дмитрий Цветцих <dmitrycvet@gmail.com>
2014-09-04 19:33:14 +0600

    [=] only one call of done callback per task

--- parallel.js
+++ parallel.js
@@ -1,17 +1,21 @@
 // based on feross/run-parallel
 module.exports = function (tasks, cb) {
-  var results, pending, errors = []
+  var results, pending, completed, errors = [];
   if (Array.isArray(tasks)) {
     results = []
     pending = tasks.length
+    completed = Array.apply(null, new Array(tasks.length))
   }
 
-  function done (err, result) {
-  	if (result) {
-    	results.push(result)
-    }
-    if (err) {
+  function done (i, err, result) {
+  	if (completed[i]) {
+  		return;
+  	}  	
+  	completed[i] = true;
+  	if (err) {
     	errors.push(err);
+    } else if (result) {
+    	results.push(result);
     }
     if (--pending === 0) {
       cb && cb(errors.length ? errors : null, results)
@@ -25,8 +29,8 @@ module.exports = function (tasks, cb) {
     cb = null
   } else {
     // array
-    tasks.forEach(function (task) {
-      task(done)
+    tasks.forEach(function (task, i) {
+      task(done.bind(undefined, i))
     })
   }
 }
