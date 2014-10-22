37ff8b233eff5b75d36b3d83384a55794de224bb
Дмитрий Цветцих <dmitrycvet@gmail.com>
2014-10-22 13:21:07 +0600

    [=] refactoring

--- index.js
+++ index.js
@@ -17,97 +17,90 @@ function Downloader(opts) {
 	this.tasks = [];
 }
 
-Downloader.prototype = function() {
-
-	var scope = {
-		constructor: Downloader
-	};
-
-	var generator = function(url, opts) {
-		// task func
-		return function(cb) {
-			var memstream = new MemoryStream()
-				, name = null
-				;
-
-			memstream.on('finish', function() {
-				cb(null, {url:url, filename:name, content:memstream.toBuffer()});
-			});
-
-			request.get(url, opts)
-			.on("error", function(err){
-				cb({url:url, error:err});
-			})
-			.on('response', function (res) {
-				if (res.statusCode < 200 || res.statusCode >= 300) {
-					return cb({url:url, error: new Error(res.statusCode)});
-				}
-
-				if (res.headers.hasOwnProperty("content-disposition")) {
-					var attach = res.headers["content-disposition"];
-					var posLeft = attach.indexOf('"');
-
-					name = attach.slice(posLeft+1, -1);
-				}
-
-				res.pipe(memstream);
-			})
+// generator of tasks
+var generator = function(url, opts) {
+	// task func
+	return function(cb) {
+		var memstream = new MemoryStream()
+			, name = null
 			;
-		};
+
+		memstream.on('finish', function() {
+			cb(null, {url:url, filename:name, content:memstream.toBuffer()});
+		});
+
+		request.get(url, opts)
+		.on("error", function(err){
+			cb({url:url, error:err});
+		})
+		.on('response', function (res) {
+			if (res.statusCode < 200 || res.statusCode >= 300) {
+				return cb({url:url, error: new Error(res.statusCode)});
+			}
+
+			if (res.headers.hasOwnProperty("content-disposition")) {
+				var attach = res.headers["content-disposition"];
+				var posLeft = attach.indexOf('"');
+
+				name = attach.slice(posLeft+1, -1);
+			}
+
+			res.pipe(memstream);
+		})
+		;
 	};
+};
 
-	/* @typedef {string} Url */
+/* @typedef {string} Url */
 
-	/**
-	 * @param {Url|Url[]} url
-	 * @param {object} [opts] - Options for <request>
-	 */
-	scope.get = function(url, opts) {
-		opts = assign({}, this.opts, opts);
+/**
+ * @param {Url|Url[]} url
+ * @param {object} [opts] - Options for <request>
+ */
+Downloader.prototype.get = function(url, opts) {
+	opts = assign({}, this.opts, opts);
 
-		if (typeof url === "string") {
-			this.tasks.push( generator(url, opts) );
-		} else if (Array.isArray(url)) {
-			url.forEach(function(item){
-				this.tasks.push( generator(item, opts) );
-			}, this);
-		} else {
-			throw new TypeError("Argiment 1: expected string or array");
-		}
+	if (typeof url === "string") {
+		this.tasks.push( generator(url, opts) );
+	} else if (Array.isArray(url)) {
+		url.forEach(function(item){
+			this.tasks.push( generator(item, opts) );
+		}, this);
+	} else {
+		throw new TypeError("Argiment 1: expected string or array");
+	}
 
-		return this;
-	};
+	return this;
+};
 
-	/**
-	 * @typedef ResultHash
-	 * @type {object}
-	 * @property {Url} url
-	 * @property {Buffer} content - Downloaded data
-	 * @property {null|String} filename - File name from `content-disposition` header
-	 */
+/**
+ * @typedef ResultHash
+ * @type {object}
+ * @property {Url} url
+ * @property {Buffer} content - Downloaded data
+ * @property {null|String} filename - File name from `content-disposition` header
+ */
 
-	 /**
-	  * @typedef ErrorHash
-	  * @type {object}
-	  * @property {Url} url
-	  * @property {Error} error
-	  */
+ /**
+  * @typedef ErrorHash
+  * @type {object}
+  * @property {Url} url
+  * @property {Error} error
+  */
 
-	/**
-	 * @callback RunCallback
-	 * @param {null|ErrorHash[]} err - Array of errors
-	 * @param {ResultHash[]} result 
-	 */
+/**
+ * @callback RunCallback
+ * @param {null|ErrorHash[]} err - Array of errors
+ * @param {ResultHash[]} result 
+ */
 
-	/**
-	 * @param {RunCallback} cb
-	 */
-	scope.run = function(cb) {
-		parallel(this.tasks, cb);
-	};
+/**
+ * @param {RunCallback} cb
+ */
+Downloader.prototype.run = function(cb) {
+	parallel(this.tasks, cb);
+};
 
-	return scope;
-}.call(null);
 
 /**
  * Simple parallel downloader
